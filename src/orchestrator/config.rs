35b9fd782260aeb9ced71f7f6d27b1d5554837e1
enfmarinho <eduardomarinho@pm.me>
2026-01-29 14:36:05 -0300

    Add material count target distributions

--- src/orchestrator/config.rs
+++ src/orchestrator/config.rs
@@ -1,7 +1,12 @@
 // Default values
+use std::sync::OnceLock;
 
 /// Network
 pub const HIDDEN_LAYER_SIZE: usize = 1024;
+pub const N_OUTPUT_BUCKETS: usize = 8;
+pub const QA: i16 = 255;
+pub const QB: i16 = 64;
+pub const QAB: i16 = QA * QB;
 
 /// DataFilter
 pub const MIN_PLY: u16 = 16;
@@ -9,6 +14,7 @@ pub const MAX_SCORE: u16 = 5000;
 pub const EXCLUDE_IN_CHECK: bool = true;
 pub const EXCLUDE_SPECIAL_MOVES: bool = true;
 pub const EXCLUDE_CAPTURE: bool = true;
+pub const OUT_BUCKET_COUNT_FILTER: bool = true;
 
 /// DataLoader
 pub const BUFFER_SIZE_MB: usize = 2048;
@@ -34,3 +40,35 @@ pub const TRAIN_FINAL_LR: f32 = 1e-5;
 pub const TUNE_END_SUPERBATCH: usize = 200;
 pub const TUNE_INITIAL_LR: f32 = 1e-4;
 pub const TUNE_FINAL_LR: f32 = 0.;
+
+/// Outbut buckets material count target distribution
+pub const CHESS_PIECE_COUNT: usize = 32;
+
+fn material_count_target_distribution() -> [f64; CHESS_PIECE_COUNT] {
+    const SIGMA: f64 = 16.0;
+    const MU: f64 = (CHESS_PIECE_COUNT - 1) as f64 / 2.0;
+
+    let mut weights = [0.; CHESS_PIECE_COUNT];
+    let mut sum = 0.0;
+
+    // unnormalized Gaussian distribution
+    for i in 0..CHESS_PIECE_COUNT {
+        let x = i as f64 - MU;
+        let v = (-x * x / (2.0 * SIGMA * SIGMA)).exp();
+        weights[i] = v;
+        sum += v;
+    }
+
+    // normalize Gaussian distribution
+    for v in &mut weights {
+        *v /= sum;
+    }
+
+    weights
+}
+
+
+pub fn get_material_count_target(index: usize) -> f64 {
+    static MATERIAL_COUNT_TARGET_DISTRIBUTION: OnceLock<[f64; 32]> = OnceLock::new();
+    MATERIAL_COUNT_TARGET_DISTRIBUTION.get_or_init(material_count_target_distribution)[index]
+}
